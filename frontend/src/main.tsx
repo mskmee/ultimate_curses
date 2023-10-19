7ccd126777268beb424bbbc9d6855de64a2f3e9c
Maksim Maksimenko <85788646+mskmee@users.noreply.github.com>
2023-10-19 14:31:26 +0300

    uc-1: * eslint

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,11 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.querySelector('#root') as HTMLElement).render(
   <React.StrictMode>
     <Suspense fallback={<h1>LOADING...</h1>}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <BrowserRouter></BrowserRouter>
     </Suspense>
   </React.StrictMode>,
 );
