8881eb74ca2ea741cb32ad3bb49733a214697c90
Maksim Maksimenko <85788646+mskmee@users.noreply.github.com>
2023-10-20 10:34:42 +0300

    uc-4: + browser theme detector

--- frontend/src/app/providers/theme-provider/ui/theme-provider.tsx
+++ frontend/src/app/providers/theme-provider/ui/theme-provider.tsx
@@ -2,10 +2,12 @@ import { useMemo, useState } from '~/shared/lib/hooks/hooks.js';
 import { storage, StorageKey } from '~/shared/model/storage/storage.js';
 import { type FC, type PropsWithChildren } from '~/shared/types/types.js';
 
-import { Theme, ThemeContext, type ThemeType } from '../model/model.js';
+import { getThemeFromBrowser } from '../lib/get-theme-from-browser.js';
+import { ThemeContext, type ThemeType } from '../model/model.js';
 
 const ThemeProvider: FC<PropsWithChildren> = ({ children }) => {
-  const storedTheme = storage.get<ThemeType>(StorageKey.THEME) || Theme.LIGHT;
+  const storedTheme =
+    storage.get<ThemeType>(StorageKey.THEME) || getThemeFromBrowser();
 
   const [theme, setTheme] = useState(storedTheme);
 
